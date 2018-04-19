096b73370278d0673960123ab34422da8ca59dc8
Brent Bauman <brentbauman86@gmail.com>
2018-04-19 22:36:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,7 +28,7 @@ end
 def find_a(words)
   starts_with = []
   words.find do |a|
-    if  a == starts_with << a.start_with?("a")
+    if  a == a.start_with?("a")
     return starts_with
   else
   false
