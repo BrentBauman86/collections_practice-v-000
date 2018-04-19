c5e5562f7770afa0209ea31ff45fe8b6755db5c5
Brent Bauman <brentbauman86@gmail.com>
2018-04-19 23:58:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,8 +28,7 @@ end
 def find_a(words)
   starts_with = []
   words.find do |a| starts_with << a.start_with?("a")
-    if a.start_with?("a") 
-      starts_with << a.start_with?("a")
+    if a.start_with?("a")
      return starts_with
   else
  return  false
